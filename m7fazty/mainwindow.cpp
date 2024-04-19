dba90c94db4993d67c93680bcdfaf61e617fa9c7
Menna-sokkar <mennasokkr0987@gmail.com>
2024-04-19 19:07:17 +0200

    login page design

--- m7fazty/mainwindow.cpp
+++ m7fazty/mainwindow.cpp
@@ -1,12 +1,14 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->label->setHidden(1);
+    QPixmap background("D:/Projects/2nd Year/DS/Screenshot 2024-04-17 004234.png");
+    ui->label_bg->setPixmap(background);
 
 }
 
@@ -17,7 +19,12 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    ui->label->setHidden(0);
-    ui->label->setText("hello");
+
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+
 }
 
