14a486b20d38cfdd346d8d55cad00eb0b5293307
Menna-sokkar <mennasokkr0987@gmail.com>
2024-04-24 01:55:42 +0200

    .

---
+++ m7fazty/account.h
@@ -0,0 +1,19 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include <string>
+using namespace std;
+
+class account
+{
+public:
+    string username;
+    string password;
+    string address;
+    int age;
+    bool status;
+
+    account();
+    //string change_password(string);
+};
+
+#endif // ACCOUNT_H
