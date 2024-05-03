4b6d8c40b53df967deece39957d6961992c2e27d
Menna-sokkar <mennasokkr0987@gmail.com>
2024-05-03 05:47:37 +0300

    .

--- m7fazty/mainwindow.cpp
+++ m7fazty/mainwindow.cpp
@@ -4,66 +4,52 @@
 #include <fstream>
 #include <filesystem>
 #include <iostream>
-#include"files.h"
-#include"requestmoney_dialog.h"
+#include "files.h"
+#include "requestmoney_dialog.h"
 #include "sendmoney_dialog.h"
+
 using namespace std;
 namespace fs = std::filesystem;
 
 QStackedWidget* MainWindow::stackedWidget = nullptr;
 
-
-
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
     stackedWidget = new QStackedWidget(this);
-    // Add the main window UI created with Qt Designer to the stacked widget
-    stackedWidget->addWidget(ui->centralwidget);
-
-    // Set the central widget to the stacked widget
-    setCentralWidget(stackedWidget);
-
-    stackedWidget = new QStackedWidget(this);
     stackedWidget->addWidget(ui->centralwidget);
     setCentralWidget(stackedWidget);
-    stackedWidget->show();
 
     loginWidget = new Login();
     stackedWidget->addWidget(loginWidget);
+
 
     files::read_from_file("D:/Projects/2nd Year/DS/m7fazty/m7fazty/files/Transiction.csv");
     for (unordered_map<string, transiction*>::value_type & trans : requestMoney_dialog::trans_read) {
         transiction* t = trans.second;
         cout << trans.first << " " << t->receiver << " " << t->sender << " " << t->amount << " " << t->date << " " << t->time << " " << t->status << endl;
-        requestMoney_dialog::usedIDs.insert( trans.first);
-        sendMoney_dialog::usedIDs.insert( trans.first);
+        requestMoney_dialog::usedIDs.insert(trans.first);
+        sendMoney_dialog::usedIDs.insert(trans.first);
     }
     cout << requestMoney_dialog::trans_read.size() << endl;
-
 }
-
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
-
 void MainWindow::on_login_clicked()
 {
-
     stackedWidget->setCurrentWidget(loginWidget);
     requestMoney_dialog::trans_data.clear();
 }
-
 
 void MainWindow::on_sign_up_clicked()
 {
     signup_Widget = new sign_up();
     stackedWidget->addWidget(signup_Widget);
     stackedWidget->setCurrentWidget(signup_Widget);
-
 }
 
--- m7fazty/mainwindow.h
+++ m7fazty/mainwindow.h
@@ -1,16 +1,18 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-#include <QMainWindow>
+
 #include <QStackedWidget>
 #include "Login.h"
-#include"sign_up.h"
+#include "sign_up.h"
+#include <QLabel>
+#include <QMainWindow>
+
 namespace fs = std::filesystem;
 using namespace std;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
-
 
 class MainWindow : public QMainWindow
 {
@@ -21,17 +23,16 @@ public:
     ~MainWindow();
     QStackedWidget static *stackedWidget;
 
-
-
 private slots:
     void on_login_clicked();
-
     void on_sign_up_clicked();
 
 private:
     Ui::MainWindow *ui;
     Login *loginWidget;
-    sign_up * signup_Widget;
+    sign_up *signup_Widget;
+
+
 
 };
 
